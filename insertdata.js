8e3a2ef4e25eeb5464348ca7c4ed36e22ec67850
Chike <chike.onuorah@gmail.com>
2017-06-21 09:32:33 -0700

    add insert.js file for local data insertion

--- insertdata.js
+++ insertdata.js
@@ -3,6 +3,7 @@ var db = require('./2back-end/dbconfig.js');
 var Review = require('./2back-end/models.js');
 mongoose.Promise = require('bluebird');
 
+// Stock Dummy Data
 
 var nailSalonReview = {'name': 'Dummy Nail Salon Name', 'review': 'Dummy Nail Salon Review', 'rating': 'Dummy Nail Salon Rating', 'category': 'Nail Salons'};
 
@@ -10,29 +11,69 @@ var barberShopReview = {'name': 'Dummy Barber Shop Name', 'review': 'Dummy Barbe
 
 var massageParlorReview = {'name': 'Dummy Massage Parlor Name', 'review': 'Dummy Massage Parlor Review', 'rating': 'Dummy Massage Parlor Rating', 'category': 'Massage Parlors'};
 
-var allData = [nailSalonReview, barberShopReview, massageParlorReview];
+var stockData = [nailSalonReview, barberShopReview, massageParlorReview];
+
+
+// 3 Silly Dummy Reviews
+
+var sillyNailReview = new Review({
+  'name': 'Neverland Nails', 
+  'review': 'Great Nail Salon. My feet look great!', 
+  'rating': '5', 
+  'location': 'Chicago', 
+  'category': 'Nail Salon'});
+
+sillyNailReview.save();
+
+var sillyMassageReview = new Review({
+  'name': 'Manly Massages', 
+  'review': 'My back never felt better!!! These guys are legit!', 
+  'rating': '5', 
+  'location': 'Boston', 
+  'category': 'Massage Parlor'});
+
+sillyMassageReview.save();
+
+var sillyBarberShopReview = new Review({
+  'name': 'Fine Cutz', 
+  'review': 'My hair is anything but fine. Its a mess. Never Again!', 
+  'rating': '2', 
+  'location': 'San Francisco', 
+  'category': 'BarberShops'});
+
+sillyBarberShopeReview.save();
+
 
 var locations = ['Chicago', 'San Francisco', 'Boston'];
 
-var number = 1;
+var reviewNumber = 1;
 
-var insertReviews = function () {
-  for (var i = 0; i < allData.length; i++) {
+// insert multiple stock reviews in multiple locations
+
+var insertStockReviews = function () {
+  for (var i = 0; i < stockData.length; i++) {
     var randomIndex = Math.floor(Math.random() * locations.length);
     var location = locations[randomIndex];
+    var randomRating = Math.floor(Math.random() * 6);
     var rvw = new Review({
-      'name': allData[i].name + ' ' + number, 
-      'review': allData[i].review + ' ' + number, 
-      'rating': allData[i].rating + ' ' + number, 
+      'name': allData[i].name + ' ' + reviewNumber, 
+      'review': allData[i].review + ' ' + reviewNumber, 
+      'rating': randomRating, 
       'location': location, 
       'category': allData[i].category});
     rvw.save();
-    number++;
-    console.log(number);
+    reviewNumber++;
+    console.log(reviewNumber);
   }
 };
 
 for (var i = 0; i < 5; i++) {
-  insertReviews();
-}
+  insertStockReviews();
+}  
+
+// push committed changes up
+// figure out merge conflict situation if there is one
+// check if current changes work
+// add insert command to readme file
+// figure out how to approach the sudo service mongod start command
 
